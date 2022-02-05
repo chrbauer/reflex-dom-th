d49d10fcd4606133702795f28cc8fe53b2d19adf
Christoph Bauer <mail@christoph-bauer.net>
2022-02-05 20:30:25 +0100

    parse trivial tree

--- src/Reflex/Dom/TH.hs
+++ src/Reflex/Dom/TH.hs
@@ -8,8 +8,22 @@ import Data.Void
 import Data.Text (Text)
 
 type Parser = Parsec Void String
+type Tag = String
+data Element = Element Tag [Element]
+               | Text String
+               deriving Show
 
-tag :: Parser String 
-tag =  between (char '<') (char '>') (many alphaNumChar) 
+openTag :: Parser String 
+openTag =  try $ between (char '<') (space >> char '>') (many alphaNumChar)
 
+closeTag :: String -> Parser String 
+closeTag tag =  between (string "</") (space >> char '>') (string tag) 
+
+
+element :: Parser Element
+element = do
+  tag <- openTag
+  childs <- many element
+  closeTag tag
+  return $ Element tag childs
 
