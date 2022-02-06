d98cda8091ddb100de1a66bd546d6a53ce9bed14
Christoph Bauer <mail@christoph-bauer.net>
2022-02-06 13:34:44 +0100

    instantiate template

--- src/Reflex/Dom/TH.hs
+++ src/Reflex/Dom/TH.hs
@@ -10,12 +10,15 @@ import Language.Haskell.TH.Quote
 
 import Reflex.Dom.TH.Parser
 
+instantiate  (Element name []) = [| el name $ blank |]
+
 dom :: QuasiQuoter
 dom = QuasiQuoter
   { quoteExp  = \str ->
       case parseTemplate "" str of
         Left err -> fail $ show err
-        Right x  -> [| el "div" |]
+        Right x  ->  instantiate x
   , quotePat  = error "Usage as a parttern is not supported"
   , quoteType = error "Usage as a type is not supported"
+  , quoteDec = error "Usage as a decl is not supported"
   }
